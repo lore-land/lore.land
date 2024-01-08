e46006586944fc8fdfae05cec5b45a38636ee47b
Sam Washington <sam@spwashi.com>
2024-01-08 16:34:55 -0600

    character development: mood

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 let currentFrame = 1;
 const frameOptions = ["0.css", "1.css"];
-const moods = ["tldr"];
+const moods = {
+  tldr: "tldr",
+};
 
 beginOscillation();
 listenToMood();
@@ -8,10 +10,29 @@ listenToMood();
 function listenToMood() {
   const moodSelect = document.querySelector("#mood-select");
   const stylesheet = document.querySelector("#mood-styles");
+
+  const starterMood = new URLSearchParams(window.location.search).get("mood");
+  setDocumentMood(starterMood);
+
   moodSelect.addEventListener("change", (event) => {
     let value = event.target.value;
-    stylesheet.href = `/css/moods/${value}.css`;
+    let selection = moods[value];
+    setDocumentMood(selection);
   });
+
+  function setDocumentMood(mood = starterMood) {
+    document.body.dataset.mood = mood;
+    stylesheet.href = `/css/moods/${mood}.css`;
+    adjustLinks(mood);
+  }
+
+  function adjustLinks(mood) {
+    const links = document.querySelectorAll('a[href*="/book/chapter/"]');
+    links.forEach((link) => {
+      link.href =
+        link.href.split("?")[0].replace(/\/*$/, "") + `/?mood=${mood}`;
+    });
+  }
 }
 
 function beginOscillation() {
