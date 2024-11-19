7cfae52995f3264a37fb76ef0378f8bcca0ae23a
spwashi <sam@spwashi.com>
2024-11-19 15:14:53 -0600

    normalization

--- js/page.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#lore-button").addEventListener("click", () => {
-  const pageData = {
-    nodes: [],
-    links: [],
-  };
-  navigator.clipboard.writeText(JSON.stringify(pageData)).then(() => {
-    alert("copied to clipboard");
-  });
-});
