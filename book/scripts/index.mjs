d14efd9958b9da770d1bd768a3293802fdde2fba
spwashi <sam@spwashi.com>
2024-11-19 15:14:53 -0600

    normalization

---
+++ book/scripts/index.mjs
@@ -0,0 +1,12 @@
+import {CustomBoof} from "./custom-boof.mjs";
+import {CustomBoonberry} from "./custom-boonberry.mjs";
+import {CustomFool} from "./custom-fool.mjs";
+import {CustomPuzzle} from "./custom-puzzle.mjs";
+import {CustomBonk} from "./custom-bonk.mjs";
+
+// Register the custom elements
+customElements.define('custom-boof', CustomBoof);
+customElements.define('custom-boonberry', CustomBoonberry);
+customElements.define('custom-fool', CustomFool);
+customElements.define('custom-puzzle', CustomPuzzle);
+customElements.define('custom-bonk', CustomBonk);
